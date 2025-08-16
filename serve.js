3977a36073f74d1cb4021aa9d5676afe2d1c85cc
paulo borges de almeida <borgespaulo72@yahoo.com.br>
2025-08-16 07:52:33 -0300

    fix: fixes

--- serve.js
+++ serve.js
@@ -1,20 +1,82 @@
 import express from 'express'
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 const port = 3000
-const users = []
 
 
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
+// borges
+// yBgacQbNPjkzD1xt 
+
+// Colocar ou postar no banco de dados
+app.post('/usuarios', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
     res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req, res) => {
+
+// listar ou mostrar o que tem no banco de dados 
+app.get('/usuarios', async (req, res) => {
+    let users = []
+
+    // filtro só pelo nome 
+    if (req.query) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
+            }
+        })
+    } else {
+        users = await prisma.user.findMany()
+    }
+
     res.status(200).json(users)
 })
+
+// Atualizar no banco de dados
+app.put('/usuarios/:id', async (req, res) => {
+    await prisma.user.update({
+
+        where: {
+            id: req.params.id
+        },
+
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+
+// Deletar no banco de dados
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json('message: "Usuario deletado com SUCESSO!"')
+})
+
+
 
 app.listen(port, () => {
     console.log(`servidor Rodando na porta: ${port}`)
